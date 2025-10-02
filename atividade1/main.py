b01d7b6f097de968c865908d6f8cc505797bcc10
Gustavo Saraiva <gsaraivam10@gmail.com>
2025-10-01 21:22:23 -0300

    teste codewise nova branch

--- atividade1/main.py
+++ atividade1/main.py
@@ -29,7 +29,7 @@ async def subtrair_numeros(requisicao: OperacaoRequest): #mesma lógica da soma
 #====================================
 
 
-########## MULTIPLICAÇÃO #########
+########## MULTIPLICAÇÃO #############
 @app.post("/multiplicar", response_model=OperacaoResponse, summary="Multiplicação de dois números", response_description="Resultado da multiplicação", tags=["Operações"])
 async def multiplicar_numeros(requisicao: OperacaoRequest): #mesma lógica da soma
     resultado = multiplicar(requisicao.a, requisicao.b) #chama a função multiplicar que está no services/operations.py
@@ -37,7 +37,7 @@ async def multiplicar_numeros(requisicao: OperacaoRequest): #mesma lógica da so
 #====================================
 
 
-########## DIVISÃO #########
+########## DIVISÃO #############
 @app.post("/dividir", response_model=OperacaoResponse, summary="Divisão de dois números", response_description="Resultado da divisão", tags=["Operações"])
 async def dividir_numeros(requisicao: OperacaoRequest): #mesma lógica da soma
     try: #tenta executar o código dentro do bloco try, se der erro, ele pula para o bloco except (msm coisa de poo)
